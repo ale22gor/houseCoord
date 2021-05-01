97171bb452554ec75f92c85bf58cd79e9ba27dcf
Dmitry <dmitryegorenko@gmail.com>
2021-05-01 22:46:53 +0300

    Update roadHouse.py

--- roadHouse.py
+++ roadHouse.py
@@ -2,9 +2,23 @@ import sys, json
 import requests
 import np
 
-x=sys.argv[1]
-y=sys.argv[2]
-r = requests.get('https://nominatim.openstreetmap.org/search.php?q='+x+'%2C'+y+'&polygon_geojson=1&format=jsonv2')
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+if is_number(sys.argv[1]) and is_number(sys.argv[2]):
+  x=sys.argv[1]
+  y=sys.argv[2] 
+  r = requests.get('https://nominatim.openstreetmap.org/search.php?q='+x+'%2C'+y+'&polygon_geojson=1&format=jsonv2')
+else:
+  print('https://geocode-maps.yandex.ru/1.x/?geocode='+'+'.join(sys.argv[1:]))
+  r = requests.get('https://geocode-maps.yandex.ru/1.x/?geocode='+'+'.join(sys.argv[1:]))
+  print('wrong')
+
 coordinates = r.json()[0]['geojson']['coordinates']
 for pair in coordinates[0]:
     pair[0], pair[1] = pair[1], pair[0]
